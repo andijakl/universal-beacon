1a0f7c39a088e628586006803c2eeb41f47db946
Andreas Jakl <andreas.jakl@live.com>
2017-04-04 07:26:57 +0200

    Update nuspec file for 2.0 library release. Use Nuget package in example app.

--- UniversalBeaconLibrary/NuGetPush.bat
+++ UniversalBeaconLibrary/NuGetPush.bat
@@ -1,1 +1,3 @@
-nuget push ./nupkg/UniversalBeaconLibrary.1.8.1.nupkg
+nuget.exe setApiKey [apikey]
+nuget push ./nupkg/UniversalBeaconLibrary.2.0.0.nupkg -Source https://www.nuget.org/api/v2/package
+rem nuget push ./nupkg/UniversalBeaconLibrary.2.0.0.symbols.nupkg -source https://nuget.smbsrc.net/
