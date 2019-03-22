fb67aa76140e8eca8aa0e00537cb8903ad744c43
Andreas Jakl <andreas.jakl@live.com>
2019-03-21 17:34:37 -0700

    Fix iOS reference netstandard. Update NuGet to prepare for alpha release. Downgrade minimum Windows version.

--- Library/NuGetPush.bat
+++ Library/NuGetPush.bat
@@ -1,1 +1,1 @@
-nuget push .\nupkg\UniversalBeaconLibrary.3.2.0.nupkg -source https://www.nuget.org/api/v2/package
+nuget push .\nupkg\UniversalBeaconLibrary.4.0.0-alpha.nupkg -source https://www.nuget.org/api/v2/package
